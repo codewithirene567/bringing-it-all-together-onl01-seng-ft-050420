11d5834f8b0f5c6196972a6c22bfdf674e43c0d4
Irene Scott <irenejinscott@gmail.com>
2020-06-09 01:05:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -73,8 +73,9 @@ attr_accessor :id, :name, :breed
       VALUES (?, ?)
       SQL
       DB[:conn].execute(sql, self.name, self.breed)
-
-
+      @id= DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    end
   end
+
 
 end
