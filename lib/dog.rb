d512f0ac65344a151c211e87dee216b51d45a31b
Irene Scott <irenejinscott@gmail.com>
2020-06-09 01:18:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,7 +41,6 @@ attr_accessor :id, :name, :breed
 
   def self.create(attribute_hash)
     dog = self.new(attribute_hash)
-    binding.pry
     dog.save
     dog
   end
@@ -69,8 +68,8 @@ attr_accessor :id, :name, :breed
     dogs = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dogs.empty?
       dog_data = dogs[0]
-
       dogs = new_from_db(dog_data)
+      binding.pry
     else
       dogs = self.create(name: name, breed: breed)
     end
