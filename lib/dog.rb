eb840ca0bdd3b9430bf975b94c52a3d8c3dc2e8f
Irene Scott <irenejinscott@gmail.com>
2020-06-09 00:48:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -35,11 +35,11 @@ attr_accessor :id, :name, :breed
 
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs") [0][0]
-    
+
   end
 
   def self.create(attribute_hash)
-    dog = self.new(hash_of_attributes)
+    dog = self.new(attribute_hash
     dog.save
     dog
   end
