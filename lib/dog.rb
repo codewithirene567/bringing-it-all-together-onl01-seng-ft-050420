7ae0c9e361c3bf14ba9f17dac7a5f6c8e191de8e
Irene Scott <irenejinscott@gmail.com>
2020-06-09 00:16:16 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,49 @@
+class Dog
+attr_accessor :id, :name, :breed
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE dogs;
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    if self.id
+      self.update #if it exists then update it, otherwise create new instance with the id
+    else
+      sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.breed)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs") [0][0]
+    end
+  end
+
+  def self.create(attribute_hash)
+    dog = Dogs.new(attribute_hash)
+    dog.each {|key, value| dog.send}
+    dog
+  end
+end
